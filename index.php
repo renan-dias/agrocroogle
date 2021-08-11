96fb6f0b72f13a580611294aabdb204b2ca1faae
Renan <renanprodias2050@gmail.com>
2021-08-11 14:11:47 -0300

    project configurations

---
+++ index.php
@@ -0,0 +1,69 @@
+    <?php 
+
+    ?>
+    <!doctype html>
+    <html lang="pt-br" class="h-100">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Agrocroogle system home page">
+        <meta name="author" content="Renan Dias">
+        <title>AGROCROODLE</title>
+
+        
+
+        <!-- CSS -->
+    <link href="../agrocroogle/src/static/css/bootstrap/bootstrap.min.css" rel="stylesheet" >
+    <link href="../agrocroogle/src/static/css/pages/index.css" rel="stylesheet" >
+
+        <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="public/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="public/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="public/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="public/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="public/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="public/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="public/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="public/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="public/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="public/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="public/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="public/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="public/favicon-16x16.png">
+    <link rel="manifest" href="public/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+            
+    </head>
+
+    <body class="d-flex h-100 text-center text-white bg-dark">
+        
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="mb-auto">
+        <div>
+            <nav class="nav nav-masthead justify-content-center float-md-end">
+            <a class="nav-link active" aria-current="page" href="src/modules/login.php">Login</a>
+          </nav>
+        </div>
+      </header>
+
+      <main class="px-3">
+        <h1>AGRO<span class="google-style-blue">CR</span><span class="google-style-red">O</span><span class="google-style-yellow">O</span><span class="google-style-blue">G</span><span class="google-style-green">L</span><span class="google-style-red">E</span>
+      </h1>
+        <form class="d-flex" method="post">
+            <input class="form-control me-2" type="search" placeholder="Digite um termo" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Buscar</button>
+        </form>
+      </main>
+
+      <!-- Footer area-->
+    <footer class="mt-auto text-white-50">
+        <p>Desenvolvido por Renan Dias</p>     
+      </footer> 
+    </div>
+
+      </body>
+    </html>
+
+
---
+++ src/modules/login.php
@@ -0,0 +1,68 @@
+<?php
+
+?>
+<!doctype html>
+    <html lang="pt-br">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Agrocroogle system login page">
+        <meta name="author" content="Renan Dias">
+        <title>AGROCROODLE · Login</title>
+
+        
+
+        <!-- CSS -->
+    <link href="./static/css/bootstrap/bootstrap.min.css" rel="stylesheet" >
+    <link href="./static/css/pages/login.css" rel="stylesheet" >
+
+        <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="../agrocroogle/public/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../agrocroogle/public/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../agrocroogle/public/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../agrocroogle/public/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../agrocroogle/public/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../agrocroogle/public/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../agrocroogle/public/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../agrocroogle/public/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../agrocroogle/public/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="../agrocroogle/public/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../agrocroogle/public/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../agrocroogle/public/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../agrocroogle/public/favicon-16x16.png">
+    <link rel="manifest" href="public/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+            
+    </head>
+  <body class="text-center">
+    
+<main class="form-signin">
+  <form>
+    <img class="mb-4" src="" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+  </form>
+</main>
+
+
+    
+  </body>
+</html>
